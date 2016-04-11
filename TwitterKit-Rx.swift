350408ed12b9a8cf8ac787aa25e70ca42d5f2d9f
Patrick Balestra <me@patrickbalestra.com>
2016-04-11 21:32:36 +0200

    Removed deprecated method in TwitterKit

--- TwitterKit-Rx.swift
+++ TwitterKit-Rx.swift
@@ -34,25 +34,6 @@ public extension Twitter {
         }
     }
     
-    /// Triggers user authentication with Twitter.
-    ///
-    /// - parameter client:  API client used to load the request.
-    ///
-    /// - returns: The Guest user session.
-    public func rx_logInGuest(client: TWTRAPIClient) -> Observable<TWTRGuestSession> {
-        return Observable.create { (observer: AnyObserver<TWTRGuestSession>) -> Disposable in
-            self.logInGuestWithCompletion { session, error in
-                guard let session = session else {
-                    observer.onError(error ?? TwitterError.Unknown)
-                    return
-                }
-                observer.onNext(session)
-                observer.onCompleted()
-            }
-            return AnonymousDisposable { }
-        }
-    }
-    
     /// Loads a Twitter User.
     ///
     /// - parameter userID:  ID of the user account to be fetched.
